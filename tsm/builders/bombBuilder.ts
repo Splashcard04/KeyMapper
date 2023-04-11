43be685812492d726c493138313d703d481d4674
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-04-11 11:04:47 -0700

    finish bomb builder class

--- tsm/builders/bombBuilder.ts
+++ tsm/builders/bombBuilder.ts
@@ -1,11 +1,11 @@
 import { file } from '../map.ts'
-import{ Json, Vec2, Vec3, Vec4 } from '../types.ts'
+import{ Json, Vec1Keyframes, Vec2, Vec3, Vec3Keyframes, Vec4, Vec4Keyframes } from '../types.ts'
 
 export class bombBuilder {
     json: Json = {
         b: 0, x: 0, y: 0, customData: { animation: {} }
     } 
-    config: Json = {}
+    private config: Json = {}
 
     constructor(time: number) { this.json.b = time; return this }
 
@@ -24,9 +24,13 @@ export class bombBuilder {
 
     flip(flip: Vec2) { this.json.customData.flip = flip; return this }
     noteGravity(gravity: boolean) { if(gravity==true) { this.json.customData.disableNoteGravity = false } else { this.json.customData.disableNoteGravity = true } return this }
-    noteLook(look: boolean) { if(look == true) { this.json.customData.disableNoteLook = true } else { this.json.customData.disableNoteLook = false } return this }
+    noteLook(look: boolean) { if(look == true) { this.json.customData.disableNoteLook = false } else { this.json.customData.disableNoteLook = true } return this }
 
-    
+    animatePosition(position: Vec3Keyframes) { this.json.customData.animation.position = position; return this }
+    animateDefinitePosition(position: Vec3Keyframes) { this.json.customData.animation.definitePosition = position; return this }
+    animateDissolve(dissolve: Vec1Keyframes) { this.json.customData.animation.dissolve = dissolve; return this }
+    animateScale(scale: Vec3Keyframes) { this.json.customData.animation.size = scale; return this }
+    animateColor(color: Vec4Keyframes) { this.json.customdata.animation.color = color; return this }
 
     push() {
         if(this.config.fake === false) file.bombNotes.push(this.json)
