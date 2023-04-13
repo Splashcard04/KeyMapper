ca056c72d8092da897d288f4a3f8f1b01eaa8eb5
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-04-13 18:00:44 +0000

    environment base class + geometry light properties

--- tsm/BaseClasses/Environment.ts
+++ tsm/BaseClasses/Environment.ts
@@ -1,44 +1,33 @@
 import { activeDiff } from '../map.ts'
-import { Json, Vec1Keyframes, Vec2, Vec3, Vec3Keyframes, Vec4, shader } from '../types.ts'
+import { Json, Vec3 } from '../types.ts'
 
-type geometryType = {
-    type?: "Cube" | "Triangle" | "Quad" | "Cyliner" | "Capsule" | "Sphere",
-    material: {
-        shader: shader,
-        color?: Vec4,
-        shaderKeywords?: [],
-        track?: ""
-    } | string,
-    scale?: Vec3,
+type envType = {
+    id?: string,
+    lookup?: "Contains" | "Regex" | "Exact" | "StartsWith" | "EndsWith",
     position?: Vec3,
     rotation?: Vec3,
     localRotation?: Vec3,
+    scale?: Vec3,
     track?: string,
     lightID?: number,
     lightType?: number
+    components: {}
 }
 
-export class Geometry {
-    json: Json = {
-        geometry: {
-            material: {}
-        }
-    }
-    constructor(x: geometryType) {
-        this.json.geometry.type = x.type ?? "Cube"
-        this.json.geometry.material = x.material ?? { shader: "Standard" }
-        this.json.scale = x.scale
+export class Environment {
+    json: Json = {}
+
+    constructor(x: envType) {
+        this.json.id = x.id ?? "Environment"
+        this.json.lookup = x.lookup ?? "Contains"
         this.json.position = x.position
         this.json.rotation = x.rotation
         this.json.localRotation = x.localRotation
+        this.json.scale = x.scale
         this.json.track = x.track
         if(!this.json.components.ILightWithId) this.json.components.ILightWithId = { lightID: x.lightID ?? 0 }
         else this.json.components.ILightWithId.lightID = x.lightID
         if(!this.json.components.ILightWithType) this.json.components.ILightWithType = { lightType: x.lightType ?? 0 }
         else this.json.components.ILightWithType.lightType = x.lightType
-    }
-
-    push() {
-        activeDiff().customData.environment.push(this.json)
     }
 }
