ab81168f49f38766b4a0d20bdc24ec52c97b1060
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-05-16 16:34:12 +0000

    model animation :sob: :gun:

--- tsm/builders/modelBuilder.ts
+++ tsm/builders/modelBuilder.ts
@@ -47,5 +47,52 @@ export class modelBuilder {
         })
     }
 
+    animate(...switches: [path: string, time: number, timeEnd: number][]) {
+        switches.forEach(x => {
+            const file = JSON.parse(Deno.readTextFileSync(x[0]+'.rmmodel'))
+
+            function isAnimated(x: Json) {
+                if(typeof x[0] == 'number') {
+                    return false
+                } else {
+                    return true
+                }
+            }
+
+            file.objects.forEach((y: Json) => {
+
+
+                    const hash = [Math.floor(Math.random() * 10) * 15 - Math.floor(Math.random() * -3)].toString()
+
+                    new animateTrackBuilder(`Model${hash}`, x[1], x[2])
+                    .position([[-9999, -9999, -9999, -0.0001], y.pos, [-9999, -9999, -9999, 1.000001]])
+                    .localRotation(y.rot)
+                    .scale(y.scale)
+                    .push()
+
+                    this.a.forEach(z => {
+                        if(z.track == y.track) {
+                            z.object
+                            .position(y.pos)
+                            .localRotation(y.rot)
+                            .scale(y.scale)
+                            .track(`Model${hash}`)
+                            .push()
+                        } else {
+                            this.object
+                            .position(y.pos)
+                            .localRotation(y.rot)
+                            .scale(y.scale)
+                            .track(`Model${hash}`)
+                            .push()
+                        }
+                    })
+
+                
+            })
+        })
+    }
+
 
 }
+
