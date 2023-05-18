8024831f3a3d58575d9bdb8dfbc74aa0797a1c95
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-05-18 20:20:40 +0000

    💀

--- tsm/run.bat
+++ tsm/run.bat
@@ -1,22 +1,23 @@
 @echo off
 
-echo "Starting TSM..."
-
 IF EXIST "script.ts" (
-    echo "script.ts found..."
+    ::Literally just does nothing 💀
+    cls 
 ) ELSE (
+    ::copy the script.ts file directly from /files/script.ts into map dir
     echo "Downloading script.ts..."
     curl -o script.ts https://raw.githubusercontent.com/Splashcard04/KeyMapper/main/files/script.ts
 )
 
-cls 
-
 :loop
+:: clear console on refresh
 cls
 
+:: run the script
 echo "Restarting TSM..."
 Deno run --no-check --allow-all script.ts
+::await user input to refresh again
 pause
 
-
+::activate the loop
 goto loop
