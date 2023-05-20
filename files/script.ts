d73f3e79ca0f0aa500fcc7d2134814243456477b
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-05-20 07:32:37 -0700

    Change link to support beta

--- files/script.ts
+++ files/script.ts
@@ -1,4 +1,4 @@
-import { Map } from 'https://deno.land/x/keymapper@0.1.0/tsm/main.ts';
+import { Map } from 'https://deno.land/x/keymapper@0.0.0/tsm/main.ts';
 
 //#region Map initialization
 const map = new Map("ExpertPlusLawless", "ExpertPlusStandard")
