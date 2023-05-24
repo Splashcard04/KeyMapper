fbfe274423226382b5acca9dc23bd630f5c7b678
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-05-24 20:03:47 +0000

    'neutral' option for logging

--- src/internal.ts
+++ src/internal.ts
@@ -4,11 +4,12 @@ import { Json } from './types.ts'
   This file is full of functions to be used internally throughought the library
 */
 
-export function log(message: string, errorValue?: 'error' | 'warn' | 'success') {
-    let color;
+export function log(message: string, errorValue?: 'error' | 'warn' | 'success' | 'neutral') {
+    let color = ''
     if(errorValue == 'error') color = `\x1b[1m\x1b[31m`
     if(errorValue == 'warn') color = `\x1b[1m\x1b[33m`
     if(errorValue == 'success') color = `\x1b[1m\x1b[32m`
+
     console.log(`${color}[KeyMapper]: ${message}\x1b[1m\x1b[37m`)
 }
 
